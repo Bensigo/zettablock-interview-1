6fcacb8cad92217c54e44249bc10afd951015abb
bensigo <sigocode@gmail.com>
2022-09-29 05:39:36 +0400

    feat: update

--- src/chat/config/configuration.ts
+++ src/chat/config/configuration.ts
@@ -1,5 +1,5 @@
 export const configuration = (() => ({
-  // using this as a test api key
+  // using this as a test api key, api key should not be used or kept for prodcution for security reason
   apiKey:
     process.env.API_KEY ||
     '4WHH47R57T147NQVXWJZ07II3SM1PKESEX4RXV1NAUCVVI3BIY03RDLUZ9DUBBVXOCF6RDWEVJSTZCF9',
